442964a7b7e29f9df1af9bfc1b7d51deecc06f96
Shohei Shibata <shohei.shibata.dev@gmail.com>
2022-09-04 07:16:43 -0400

    add Netlify function to build daily

---
+++ netlify/functions/rebuild/rebuild.js
@@ -0,0 +1,18 @@
+const { schedule } = require('@netlify/functions');
+const fetch = require('node-fetch');
+
+const REBUILD_URL = process.env.REBUILD_URL;
+
+// To learn about scheduled functions and supported cron extensions,
+// see: https://ntl.fyi/sched-func
+module.exports.handler = schedule('@daily', async (event) => {
+  const eventBody = JSON.parse(event.body);
+	console.log('Running function: rebuild.js');
+  console.log(`Next function run at ${eventBody.next_run}.`);
+
+	await fetch(REBUILD_URL, { method: 'POST' });
+
+  return {
+    statusCode: 200,
+  };
+});
