bbe8b129ea7d9a3672a205b14b386a7119ab6bf5
Shohei Shibata <shohei.shibata.dev@gmail.com>
2022-09-13 08:33:44 -0400

    Rebuild function modified to run at 5am UTC daily

--- netlify/functions/rebuild/rebuild.js
+++ netlify/functions/rebuild/rebuild.js
@@ -5,12 +5,12 @@ const REBUILD_URL = process.env.REBUILD_URL;
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
-module.exports.handler = schedule('@daily', async (event) => {
+module.exports.handler = schedule('0 5 * * *', async (event) => {
   const eventBody = JSON.parse(event.body);
-	console.log('Running function: rebuild.js');
-  console.log(`Next function run at ${eventBody.next_run}.`);
+    console.log('Running function: rebuild.js');
+    console.log(`Next function run at ${eventBody.next_run}.`);
 
-	await fetch(REBUILD_URL, { method: 'POST' });
+    await fetch(REBUILD_URL, { method: 'POST' });
 
   return {
     statusCode: 200,
