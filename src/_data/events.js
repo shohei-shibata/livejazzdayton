1751b397eaa0e1298dc94761f4f49972c0143adb
Shohei Shibata <shohei.shibata.dev@gmail.com>
2022-09-22 08:16:47 -0400

    parses event description as markdown and converts to html. calendar link fix

--- src/_data/events.js
+++ src/_data/events.js
@@ -29,65 +29,65 @@ const getDateString = (value = new Date()) => {
 module.exports = async function() {
 		const cards = await getAllApprovedCards();
     return await Promise.all(cards.map(async card => {
-        const { 
-					cardId,
-					name,
-					imageId,
-					start, 
-					end, 
-					locationName, 
-					locationAddress, 
-					description,  
-					artists,
-					links
-        } = await parseCard(card)
-        const imageUrl = await getImageUrl(cardId, imageId);
-        const imageOptions = { 
-            formats: "jpeg",
-            widths: [600, 900, 1500],
-            urlPath: "/images/",
-            outputDir: "./build/images/",
-        };
+      const { 
+        cardId,
+        name,
+        imageId,
+        start, 
+        end, 
+        locationName, 
+        locationAddress, 
+        description,  
+        artists,
+        links
+      } = await parseCard(card)
+      const imageUrl = await getImageUrl(cardId, imageId);
+      const imageOptions = { 
+          formats: "jpeg",
+          widths: [600, 900, 1500],
+          urlPath: "/images/",
+          outputDir: "./build/images/",
+      };
 
-        const metadata = await Image(imageUrl, imageOptions);
-    
-        let imageAttributes = {
-            alt: "ALT TEXT",
-            sizes: "(min-width: 1024px) 100vw, 50vw",
-            loading: "lazy",
-            decoding: "async",
-        };
+      const metadata = await Image(imageUrl, imageOptions);
+  
+      let imageAttributes = {
+          alt: "ALT TEXT",
+          sizes: "(min-width: 1024px) 100vw, 50vw",
+          loading: "lazy",
+          decoding: "async",
+      };
 
-        const imageHtml = Image.generateHTML(metadata, imageAttributes);
+      const imageHtml = Image.generateHTML(metadata, imageAttributes);
 
-        const queryString = locationAddress ? 
-            `${locationName}, ${locationAddress}`
-            :
-            `${locationName} near Dayton, Ohio`;
+      const queryString = locationAddress ? 
+          `${locationName}, ${locationAddress}`
+          :
+          `${locationName} near Dayton, Ohio`;
 
-        const slug = `${getDateString(start)}-${slugify(card.name)}`
-        
-        const eventFormatted = {
-            slug: slug,
-            title: name,
-            start: start,
-            end: end,
-            location: {
-                name: locationName,
-                address: locationAddress
-            },
-            links: {
-                facebook: links.facebook && links.facebook.length > 0 ? links.facebook : null,
-                website: links.website && links.website.length > 0 ? links.website : null
-            },
-            description: description,
-            image: imageHtml,
-            artists: artists,
-            googleMapsUrl: `https://maps.google.com/maps?q=${queryString.replaceAll(" ", "+")}`,
-            googleMapsEmbed: `https://www.google.com/maps/embed/v1/search?q=${queryString.replaceAll(" ", "+")}&key=${GOOGLE_API_KEY}`
-        }
+      const slug = `${getDateString(start)}-${slugify(card.name)}`
+      
+      const eventFormatted = {
+          slug: slug,
+          title: name,
+          start: start,
+          end: end,
+          location: {
+              name: locationName,
+              address: locationAddress
+          },
+          links: {
+              facebook: links.facebook && links.facebook.length > 0 ? links.facebook : null,
+              website: links.website && links.website.length > 0 ? links.website : null
+          },
+          description: description,
+          image: imageHtml,
+          artists: artists,
+          googleMapsUrl: `https://maps.google.com/maps?q=${queryString.replaceAll(" ", "+")}`,
+          googleMapsEmbed: `https://www.google.com/maps/embed/v1/search?q=${queryString.replaceAll(" ", "+")}&key=${GOOGLE_API_KEY}`
+      }
 
-        return eventFormatted;
+      return eventFormatted;
     }));
 
 }
