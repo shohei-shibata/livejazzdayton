a6b6896d46736bb8e961dc665ab37776ceae870d
Shohei Shibata <shohei.shibata.dev@gmail.com>
2022-09-26 16:15:17 -0400

    rebuild both master and staging

--- netlify/functions/rebuild/rebuild.js
+++ netlify/functions/rebuild/rebuild.js
@@ -2,6 +2,7 @@ const { schedule } = require('@netlify/functions');
 const fetch = require('node-fetch');
 
 const REBUILD_URL = process.env.REBUILD_URL;
+const REBUILD_URL_STAGING = process.env.REBUILD_URL_STAGING;
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
@@ -11,7 +12,8 @@ module.exports.handler = schedule('0 5 * * *', async (event) => {
     console.log(`Next function run at ${eventBody.next_run}.`);
 
     await fetch(REBUILD_URL, { method: 'POST' });
-
+    await fetch(REBUILD_URL_STAGING, { method: 'POST' });
+    
   return {
     statusCode: 200,
   };
