5ba9b0bd8bd15a83638ae3cc72f0a03b64f1c5d3
Shohei <shohei.shibata.dev@gmail.com>
2024-02-02 14:06:34 -0500

    Use duration to set event end time

--- src/js/trello.js
+++ src/js/trello.js
@@ -18,6 +18,7 @@ const customFieldId = {
   website: `63298de3f0b71701cee74d9b`,
   tickets: `6333522eae22aa02e35a6c7c`,
   end: `63298d1230281c0434de7e2b`,
+  duration: `65b2354348c7c7168e48ee14`,
 }
 
 const getAllCards = async (listId) => {
@@ -86,9 +87,13 @@ const getCustomFieldTextById = (card, customFieldId) => {
   return customField?.value.text
 }
 
+const getCustomFieldNumberById = (card, customFieldId) => {
+  const customField = getCustomFieldById(card, customFieldId)
+  return customField?.value.number
+}
+
 const getCustomFieldDateById = (card, customFieldId) => {
   const customField = getCustomFieldById(card, customFieldId)
-  console.log("DATE", customField?.value)
   return customField?.value.date
 }
 
@@ -126,7 +131,6 @@ const getCustomFieldByName = async (boardId, card, fieldName) => {
 }
 
 const parseEventCard = async card => {
-  console.log(card.name, card)
   const trelloParsed = {
     cardId: card.id,
     name: card.name,
@@ -137,12 +141,12 @@ const parseEventCard = async card => {
     locationAddress: await getVenueAddressById(card.id),
     start: card.due,
     end: getCustomFieldDateById(card, customFieldId.end),
+    duration: getCustomFieldNumberById(card, customFieldId.duration),
     artists: getCustomFieldTextById(card, customFieldId.artists),
     links: {
       website: getCustomFieldTextById(card, customFieldId.website),
       tickets: getCustomFieldTextById(card, customFieldId.tickets),
     },
-    streamEmbed: await getCustomFieldByName(approvedEventsBoardId, card, "Stream Embed")
   }
   return trelloParsed;
 }
