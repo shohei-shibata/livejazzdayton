82856da0683c5a387033b33e867ad1ff579fb114
Shohei <shohei.shibata.dev@gmail.com>
2024-02-04 22:06:32 -0500

    wip form to add events

---
+++ netlify/functions/createEvent.mjs
@@ -0,0 +1,168 @@
+import axios from "axios";
+export default async (req, context) => {
+  const listIdUnapprovedEvents = "6325995bfd858d013807a56d"
+  const trelloKey = process.env.TRELLO_API_KEY
+  const trelloToken = process.env.TRELLO_API_TOKEN
+  const createEventUrl = `https://api.trello.com/1/cards?idList=${listIdUnapprovedEvents}&key=${trelloKey}&token=${trelloToken}`
+  /*async function addAddress(cardId, {address, locationName}) {
+    const addAddressUrl = `https://api.trello.com/1/cards/${cardId}?key=${trelloKey}&token=${trelloToken}`
+    return await axios.put(addAddressUrl, {
+      address: address,
+      locationName: locationName
+    })
+    .then(res => {
+      console.log("ADDRESS SUCCESS")
+      return res
+    })
+    .catch(err => {
+      console.log("ADDRESS ERROR")
+      return err
+    })
+  }*/
+  async function addAttachment(cardId, { imageUrl }) {
+    const addAttachmentUrl = `https://api.trello.com/1/cards/${cardId}/attachments?key=${trelloKey}&token=${trelloToken}`
+    return await axios.post(addAttachmentUrl, {
+      url: imageUrl
+    })
+    .then(res => {
+      return res
+    })
+    .catch(err => {
+      console.log("ATTACHMENT ERROR", err)
+      return err
+    })
+  }
+  async function addCustomFields(cardId, {
+      start, 
+      artists, 
+      websiteUrl, 
+      ticketUrl, 
+      duration,
+      address,
+      locationName
+    }) {
+    const addCustomFieldsUrl = `https://api.trello.com/1/cards/${cardId}/customFields?key=${trelloKey}&token=${trelloToken}`
+    return await axios.put(addCustomFieldsUrl, {
+      customFieldItems: [
+        {
+          idCustomField: "63298cde0d12540117f3fa32",
+          value: {
+            date: start
+          }
+        },
+        {
+          idCustomField: "63298eb14e9a4702d35dbd47",
+          value: {
+            text: artists
+          }
+        },
+        {
+          idCustomField: "63298de3f0b71701cee74d9b",
+          value: {
+            text: websiteUrl
+          }
+        },
+        {
+          idCustomField: "6333522eae22aa02e35a6c7c",
+          value: {
+            text: ticketUrl
+          }
+        },
+        {
+          idCustomField: "65b2354348c7c7168e48ee14",
+          value: {
+            number: duration.toString()
+          }
+        },
+        {
+          idCustomField: "65bfd9569b44900a7431df0d",
+          value: {
+            text: `${locationName} ${address}`
+          }
+        },
+      ]
+    })
+    .then(res => {
+      return res
+    })
+    .catch(err => {
+      console.log("CUSTOM FIELDS ERROR", err)
+      return err
+    })
+  }
+  
+  const res = await req.formData().then(async data => {
+    let eventData = {}
+    for (const pair of data.entries()) {
+      if (pair[0] !== "submit") {
+        eventData = {
+          [pair[0]]: pair[1],
+          ...eventData
+        }
+      }
+    }
+    const { 
+      name, 
+      desc, 
+      address, 
+      locationName, 
+      start, 
+      artists, 
+      websiteUrl, 
+      ticketUrl, 
+      hours,
+      minutes,
+      imageUrl
+    } = eventData
+    const duration = parseInt(hours) + parseInt(minutes) / 60
+    return await axios.post(createEventUrl, {
+      name,
+      desc,
+    })
+    .then(async function (response) {
+      const secondResponse = await Promise.all([
+        /*addAddress(response.data.id, {
+          address,
+          locationName
+        }), */
+        addCustomFields(response.data.id, {
+          start,
+          artists,
+          websiteUrl,
+          ticketUrl,
+          duration,
+          address,
+          locationName
+        }),
+        addAttachment(response.data.id, {
+          imageUrl
+        })
+      ])
+      .then(res => {
+        return res
+      })
+      .catch(err => {
+        console.log("SECONDARY REQUEST ERROR", err)
+        return err
+      })
+      const error = secondResponse.find(item => item.status !== 200)
+      if (error) {
+        return error
+      }
+      return {
+        status: 200
+      }
+    })
+    .catch(function (error) {
+      console.log("ERROR", error);
+      return error
+    })
+  })
+  if (res && res.status === 200) {
+    return {
+      statusCode: 302,
+      Location: "https://google.com"
+    };
+  }
+  return new Response("ERROR");
+};
---
+++ src/events-add.md
@@ -0,0 +1,88 @@
+---
+layout: layout.liquid
+title: Add Event
+permalink: events/add/index.htm
+---
+
+# Submit a New Event
+
+Please fill out the form below. The event will be added to the site once approved by the admin.
+
+<form 
+  class="new-event-form" 
+  name="new-event-form" 
+  method="POST" 
+  action="http://localhost:8888/.netlify/functions/createEvent" 
+>
+  <input 
+    type="hidden" 
+    name="subject" 
+    value="New Event Submission" 
+  />
+  <h3>About you</h3>
+	<div class="form-field-container">
+		<label for="username">Name*: </label>
+		<input name="username" type="text" required>
+	</div>
+	<div class="form-field-container">
+		<label for="email">Email*: </label>
+		<input name="email" type="email" required>
+	</div>
+  <h3>About the event</h3>
+	<div class="form-field-container">
+		<label for="name">Title*: </label>
+		<input name="name" type="text" required>
+	</div>
+	<div class="form-field-container">
+		<label for="start">Start date/time*: </label>
+		<input name="start" type="datetime-local" required>
+	</div>
+	<div class="form-field-container">
+		<label for="hours">Duration (HH:MM): </label>
+    <div class="form-field-group-duration">
+      <input name="hours" type="number" value="2" min="0">
+      :
+      <input 
+        name="minutes" 
+        type="number" 
+        value="0" 
+        min="0" 
+        max="59"
+      >
+    </div>
+	</div>
+	<div class="form-field-container">
+		<label for="artists">Who is playing?*: </label>
+		<input 
+      name="artists" 
+      type="text" 
+      placeholder="Please separate names with a comma"
+      required
+    >
+	</div>
+	<div class="form-field-container">
+		<label for="locationName">Venue Name: </label>
+		<input name="locationName" type="text">
+	</div>
+	<div class="form-field-container">
+		<label for="address">Venue Address*: </label>
+		<input name="address" type="text" required>
+	</div>
+	<div class="form-field-container">
+		<label for="desc">Description: </label>
+		<textarea name="desc" rows="10"></textarea>
+	</div>
+	<div class="form-field-container">
+		<label for="websiteUrl">Event website link: </label>
+		<input name="websiteUrl" type="url">
+	</div>
+	<div class="form-field-container">
+		<label for="ticketUrl">Ticket purchase link: </label>
+		<input name="ticketUrl" type="url">
+	</div>
+	<div class="form-field-container">
+		<label for="imageUrl">Image URL: </label>
+		<input name="imageUrl" type="url">
+	</div>
+	<input name="submit" type="submit" value="Submit" class="btn">
+</form>
